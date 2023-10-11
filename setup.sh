1f86accc0462ba0481f0acd507e9e3f816e0483f
Maurice Garcia <mgarcia01752@outlook.com>
2023-10-11 15:57:50 -0400

    Update setup.sh PYTHON path

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ pip install argcomplete
 echo "Setup completed successfully."
 
 # Get the absolute path to the project's root directory
-ROUTER_CLI_PROJECT_ROOT="/home/dev01/Projects/Linux-Router-CLIv3"
+ROUTER_CLI_PROJECT_ROOT="/home/dev01/Projects/RouterShell"
 
 # Update the PYTHONPATH to include the project's root directory
 export PYTHONPATH="${PYTHONPATH}:${ROUTER_CLI_PROJECT_ROOT}:${ROUTER_CLI_PROJECT_ROOT}/src:${ROUTER_CLI_PROJECT_ROOT}/lib:${ROUTER_CLI_PROJECT_ROOT}/test"
