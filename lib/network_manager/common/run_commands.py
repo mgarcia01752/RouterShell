1052a90417e57f836392e1fe30fbf6735efc1d0a
Maurice Garcia <mgarcia01752@outlook.com>
2023-12-16 13:56:34 -0500

    Updating Run Command
    Signed-off-by: Maurice Garcia <mgarcia01752@outlook.com>

--- lib/network_manager/common/run_commands.py
+++ lib/network_manager/common/run_commands.py
@@ -52,46 +52,48 @@ class RunCommand:
         with open(log_path, "a") as log_file:
             log_file.write(log_entry + "\n")
     
-    def run(self, command:List[str], suppress_error:bool=False, shell=False, sudo=True) -> RunResult:
+    def run(self, command: List[str], suppress_error: bool = False, shell: bool = False, sudo: bool = True) -> RunResult:
         """
-        Run a Linux command with sudo and log the result.
+        Run a command in the Linux environment and log the result.
 
         Args:
             command (List[str]): The command and its arguments as a list.
+            suppress_error (bool, optional): If True, suppress logging of errors. Defaults to False.
+            shell (bool, optional): If True, execute the command using a shell. Defaults to False.
+            sudo (bool, optional): If True, prepend 'sudo' to the command. Defaults to True.
 
         Returns:
             RunResult: A named tuple containing stdout, stderr, exit_code, and the command.
+            
+        
         """
         try:
             if sudo:
                 command = ['sudo'] + command
-            
-            process = subprocess.run(command,   shell=shell,
-                                                check=True, 
-                                                stdout=subprocess.PIPE, 
-                                                stderr=subprocess.PIPE)
-            
+
+            process = subprocess.run(command, shell=shell, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
             exit_code = process.returncode
-            
             stdout = process.stdout.decode("utf-8")
             stderr = process.stderr.decode("utf-8")
-            
+
             cmd_str = " ".join(command)
-            
+
             self.log.info(f"run({exit_code}) -> cmd -> {cmd_str}")
-            
+
             self.log_command(cmd_str)
-            
+
             return RunResult(stdout, stderr, exit_code, command)
-        
+
         except subprocess.CalledProcessError as e:
             cmd_str = " ".join(command)
-            
+
             if not suppress_error:
-                logging.error(f"Command failed: {e}: {cmd_str}")
-                logging.error(f"Error output: {e.stderr.decode('utf-8')}")
-            
+                self.log.error(f"Command failed: {e}: {cmd_str}")
+                self.log.error(f"Error output: {e.stderr.decode('utf-8')}")
+
             self.run_cmds_failed.append(cmd_str)
             self.log_command(cmd_str)
-            
+
             return RunResult("", str(e), e.returncode, command)
+
