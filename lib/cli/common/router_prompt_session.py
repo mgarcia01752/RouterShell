d405a91b68ab4eeaa62960a920aeb9a8ef1be138
Maurice Garcia <mgarcia01752@outlook.com>
2024-05-12 23:02:54 -0600

    Continue

--- lib/cli/common/router_prompt_session.py
+++ lib/cli/common/router_prompt_session.py
@@ -3,22 +3,40 @@ from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit import print_formatted_text as print
 from common.common import Common
+from typing import Type
 
 class RouterPromptSession:
-    
-    def __init__(self):
+    """
+    Class for managing router prompt session.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes RouterPromptSession instance.
+        """
         self.top_level_commands = {}
         self.completer = WordCompleter([])
         self.history = InMemoryHistory()
         self.hostname = Common.getHostName()
 
-    def rs_prompt(self):
+    def rs_prompt(self) -> str:
+        """
+        Displays router prompt and returns user input.
+
+        Returns:
+            str: User input from the prompt.
+        """
         return prompt(f'{self.hostname}> ', completer=self.completer, history=self.history)
 
-    def register_top_level_commands(self, class_name):
-        
+    def register_top_level_commands(self, class_name: Type) -> None:
+        """
+        Registers top-level commands for the router prompt session.
+
+        Args:
+            class_name (Type): Class containing top-level commands.
+        """
         cmd_list = class_name.get_command_list()
-        
+
         for cmd in cmd_list:
             self.top_level_commands[cmd] = class_name
         
