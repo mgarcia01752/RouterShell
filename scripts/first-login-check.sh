58c5de9e481f84a2afbc402ddf893c177ee96ed6
Maurice Garcia <mgarcia01752@outlook.com>
2024-06-01 19:58:22 -0400

    Continue updating first-login script

--- scripts/first-login-check.sh
+++ scripts/first-login-check.sh
@@ -8,13 +8,9 @@ mkdir -p $FLAG_DIR
 
 # Check if the script has already been run
 if [ -f "$FLAG_FILE" ]; then
-    echo "Initial login check bypass, proceed to RouterShell"
 
-    cat /etc/sudoers
-
-    /etc/routershell/router-shell.sh
+    # /etc/routershell/router-shell.sh
     
-    echo "Coming out or RouterShell"
     exit 0
 fi
 
@@ -34,10 +30,6 @@ usermod -aG sudo $new_username
 
 # Mark the script as run
 touch $FLAG_FILE
-
-if [ -f "$FLAG_FILE" ]; then
-    echo "Initial Flag File ${FLAG_FILE} FOUND"
-fi
 
 echo "Initial setup is complete. Please log in as $new_username."
 
