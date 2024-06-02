13afe07bfc8246612e9c4fd088bcdcb21c0ec0a5
Maurice Garcia <mgarcia01752@outlook.com>
2024-06-01 20:19:04 -0400

    Continue updating first-login script

--- scripts/first-login-check.sh
+++ scripts/first-login-check.sh
@@ -15,11 +15,10 @@ mkdir -p $FLAG_DIR
 if [ -f "$FLAG_FILE" ]; then
 
     # TODO Add a bypass flag at for Debug-Version
+    toShell
 
     # /etc/routershell/router-shell.sh
 
-    toShell
-    
     exit 0
 fi
 
@@ -30,12 +29,12 @@ read -p "Enter new username: " new_username
 
 # Create the new user
 adduser $new_username
-
-# Prompt for new password for the new user
 passwd $new_username
 
 # Add new user to sudoers file
-usermod -aG sudo $new_username
+# usermod -aG sudo $new_username
+
+echo "dev01 ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
 
 # Mark the script as run
 touch $FLAG_FILE
