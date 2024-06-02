e4a98780a2a9aad074b9b96b769f3db8eeaf0354
Maurice Garcia <mgarcia01752@outlook.com>
2024-06-01 21:54:56 -0400

    Working....

--- scripts/first-login-check.sh
+++ scripts/first-login-check.sh
@@ -15,7 +15,8 @@ function configurePasswordlessSudo() {
 
     # Validate the sudoers file syntax
     if sudo visudo -cf "$sudoers_file"; then
-        # echo "Sudo configuration for $username was successful."
+        echo "Sudo configuration for $username was successful."
+
     else
         echo " ERROR: Sudo configuration for $username failed. Please check the sudoers file."
         exit 1
