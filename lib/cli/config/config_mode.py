305e28488e15418985c4c282f0bd2d5092c06b5e
Maurice Garcia <mgarcia01752@outlook.com>
2024-06-02 01:58:55 -0400

    removed Global commands in config mode

--- lib/cli/config/config_mode.py
+++ lib/cli/config/config_mode.py
@@ -10,7 +10,6 @@ class ConfigMode(ConfigurePrompt):
     def __init__(self):
         super().__init__()
 
-        self.register_top_lvl_cmds(Global())
         self.register_top_lvl_cmds(ConfigCmd())
         
         self.log = logging.getLogger(self.__class__.__name__)
